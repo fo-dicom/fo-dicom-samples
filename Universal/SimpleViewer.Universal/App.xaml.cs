fbb60f46dad12f9d17f1e48efb02010bb2b7d135
Anders Gustafsson <anders@cureos.com>
2016-01-29 12:14:15 +0100

    Added viewer stub for Universal Windows Platform

---
+++ Universal/SimpleViewer.Universal/App.xaml.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) 2012-2016 fo-dicom contributors.
+// Licensed under the Microsoft Public License (MS-PL).
+
+namespace SimpleViewer.Universal
+{
+    using System;
+    using System.Collections.Generic;
+    using Windows.ApplicationModel.Activation;
+
+    using Caliburn.Micro;
+
+    using SimpleViewer.Universal.ViewModels;
+
+    public sealed partial class App
+    {
+        private WinRTContainer container;
+
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override void Configure()
+        {
+            container = new WinRTContainer();
+            container.RegisterWinRTServices();
+
+            container.PerRequest<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            var instance = container.GetInstance(service, key);
+            if (instance != null)
+                return instance;
+            throw new Exception("Could not locate any instances.");
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            container.BuildUp(instance);
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            if (args.PreviousExecutionState == ApplicationExecutionState.Running)
+                return;
+
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
---
+++ Universal/SimpleViewer.Universal/ViewModels/ShellViewModel.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) 2012-2016 fo-dicom contributors.
+// Licensed under the Microsoft Public License (MS-PL).
+
+namespace SimpleViewer.Universal.ViewModels
+{
+    using System;
+
+    using Windows.UI.Popups;
+
+    using Caliburn.Micro;
+
+    public class ShellViewModel : PropertyChangedBase
+    {
+        string name;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                NotifyOfPropertyChange(() => Name);
+                NotifyOfPropertyChange(() => CanSayHello);
+            }
+        }
+
+        public bool CanSayHello => !string.IsNullOrWhiteSpace(this.Name);
+
+        public async void SayHello()
+        {
+            await new MessageDialog(string.Format("Hello {0}!", Name)).ShowAsync();
+        }
+    }
+}
