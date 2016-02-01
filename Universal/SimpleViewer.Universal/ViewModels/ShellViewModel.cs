7c919819384f34223fc6b4e4e5241e58fa3f8f0a
Anders Gustafsson <anders@cureos.com>
2016-02-01 17:25:53 +0100

    Open and display DICOM image

--- Universal/SimpleViewer.Universal/ViewModels/ShellViewModel.cs
+++ Universal/SimpleViewer.Universal/ViewModels/ShellViewModel.cs
@@ -4,31 +4,91 @@
 namespace SimpleViewer.Universal.ViewModels
 {
     using System;
+    using System.IO;
 
+    using Windows.Storage.Pickers;
     using Windows.UI.Popups;
+    using Windows.UI.Xaml.Media;
 
     using Caliburn.Micro;
 
+    using Dicom;
+    using Dicom.Imaging;
+
     public class ShellViewModel : PropertyChangedBase
     {
+        #region FIELDS
+
         string name;
 
-        public string Name
+        private DicomFile file;
+
+        private ImageSource image;
+
+        #endregion
+
+        #region PROPERTIES
+
+        public DicomFile File
         {
-            get { return name; }
-            set
+            get
             {
-                name = value;
-                NotifyOfPropertyChange(() => Name);
-                NotifyOfPropertyChange(() => CanSayHello);
+                return this.file;
+            }
+            private set
+            {
+                if (Equals(value, this.file))
+                {
+                    return;
+                }
+                this.file = value;
+                this.NotifyOfPropertyChange(() => this.File);
             }
         }
 
-        public bool CanSayHello => !string.IsNullOrWhiteSpace(this.Name);
-
-        public async void SayHello()
+        public ImageSource Image
         {
-            await new MessageDialog(string.Format("Hello {0}!", Name)).ShowAsync();
+            get
+            {
+                return this.image;
+            }
+            private set
+            {
+                if (Equals(value, this.image))
+                {
+                    return;
+                }
+                this.image = value;
+                this.NotifyOfPropertyChange(() => this.Image);
+            }
         }
+
+        #endregion
+
+        #region METHODS
+
+        public async void OpenFile()
+        {
+            var picker = new FileOpenPicker();
+            picker.FileTypeFilter.Add(".dcm");
+            var selected = await picker.PickSingleFileAsync();
+            if (selected == null) return;
+            
+            var stream = await selected.OpenStreamForReadAsync();
+
+            this.File = await DicomFile.OpenAsync(stream);
+            this.Image = null;
+
+            if (this.File != null)
+            {
+                var dicomImage = new DicomImage(this.File.Dataset);
+                if (dicomImage.Width > 0)
+                {
+                    this.Image = dicomImage.RenderImage().AsWriteableBitmap();
+                }
+            }
+        }
+
+        #endregion
     }
 }
