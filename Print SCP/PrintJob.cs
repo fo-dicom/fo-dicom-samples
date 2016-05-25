ee9f73ce1c06eb2d93041892438ed2d36d32b453
Anders Gustafsson <anders@cureos.com>
2016-05-25 11:21:59 +0200

    Reverted workaround, shoulb be handled by fo-dicom (#299)

--- Print SCP/PrintJob.cs
+++ Print SCP/PrintJob.cs
@@ -147,16 +147,6 @@ namespace Dicom.Printing
         {
             get
             {
-                if (!this.Contains(DicomTag.CreationDate))
-                {
-                    return DateTime.MinValue;
-                }
-
-                if (!this.Contains(DicomTag.CreationTime))
-                {
-                    return this.Get<DateTime>(DicomTag.CreationDate);
-                }
-
                 return this.GetDateTime(DicomTag.CreationDate, DicomTag.CreationTime);
             }
             set
