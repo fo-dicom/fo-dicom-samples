0dd300236f32a0aaffc3b57649aa7772884cfa15
Anders Gustafsson <anders@cureos.com>
2017-02-24 18:06:26 +0100

    Added stub for iOS sample project.

---
+++ iOS/SimpleViewer.iOS/Main.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) 2012-2017 fo-dicom contributors.
+// Licensed under the Microsoft Public License (MS-PL).
+
+using UIKit;
+
+namespace SimpleViewer.iOS
+{
+    public class Application
+    {
+        // This is the main entry point of the application.
+        static void Main(string[] args)
+        {
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            UIApplication.Main(args, null, "AppDelegate");
+        }
+    }
+}
