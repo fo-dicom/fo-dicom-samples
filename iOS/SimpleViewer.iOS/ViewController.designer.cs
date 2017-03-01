3baaf80fd3c0913123dc374de7f98faf1b52a849
Anders Gustafsson <anders@cureos.com>
2017-03-01 17:31:00 +0100

    Added simple viewing of DICOM images and attributes.

--- iOS/SimpleViewer.iOS/ViewController.designer.cs
+++ iOS/SimpleViewer.iOS/ViewController.designer.cs
@@ -1,17 +1,50 @@
-﻿//
-// This file has been generated automatically to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
 
 namespace SimpleViewer.iOS
 {
-    [Register("ViewController")]
+    [Register ("ViewController")]
     partial class ViewController
     {
-        void ReleaseDesignerOutlets()
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIImageView _imageView { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton _nextImageButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextView _textView { get; set; }
+
+        [Action ("_nextImageButton_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void NextImageButtonTouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
         {
+            if (_imageView != null) {
+                _imageView.Dispose ();
+                _imageView = null;
+            }
+
+            if (_nextImageButton != null) {
+                _nextImageButton.Dispose ();
+                _nextImageButton = null;
+            }
+
+            if (_textView != null) {
+                _textView.Dispose ();
+                _textView = null;
+            }
         }
     }
 }
