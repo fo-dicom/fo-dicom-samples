591c8b63d670c30c6de92cd5448b9eba4f4edac5
Reinhard Gruber <reinhardgruber@aon.at>
2017-06-09 01:05:10 +0200

    cfind studies sample

---
+++ Desktop/QueryRetrieve SCU/Program.cs
@@ -0,0 +1,105 @@
+﻿using Dicom;
+using Dicom.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryRetrieve_SCU
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new DicomClient();
+            client.NegotiateAsyncOps();
+
+            // Find a list of Studies
+            var request = CreateStudyRequestByPatientName("Test*^*");
+
+            request.OnResponseReceived += HandleResponse;
+            client.AddRequest(request);
+            client.Send("www.dicomserver.co.uk", 104, false, "FODICOMSCU", "STORESCP");
+
+            Console.ReadLine();
+        }
+
+        public static DicomCFindRequest CreateStudyRequestByPatientName(string patientName)
+        {
+            var request = new DicomCFindRequest(DicomQueryRetrieveLevel.Study);
+
+            request.Dataset.AddOrUpdate(new DicomTag(0x8, 0x5), "ISO_IR 100");
+
+            // add the dicom tags with empty values that should be included in the result
+            request.Dataset.AddOrUpdate(DicomTag.StudyDate, "");
+            request.Dataset.AddOrUpdate(DicomTag.StudyTime, "");
+            request.Dataset.AddOrUpdate(DicomTag.AccessionNumber, "");
+            request.Dataset.AddOrUpdate(DicomTag.ModalitiesInStudy, "");
+            request.Dataset.AddOrUpdate(DicomTag.StudyDescription, "");
+            request.Dataset.AddOrUpdate(DicomTag.InstitutionName, "");
+            request.Dataset.AddOrUpdate(DicomTag.ReferringPhysicianName, "");
+            request.Dataset.AddOrUpdate(DicomTag.ProtocolName, "");
+            request.Dataset.AddOrUpdate(DicomTag.PatientName, "");
+            request.Dataset.AddOrUpdate(DicomTag.PatientID, "");
+            request.Dataset.AddOrUpdate(DicomTag.PatientBirthDate, "");
+            request.Dataset.AddOrUpdate(DicomTag.PatientSex, "");
+            request.Dataset.AddOrUpdate(DicomTag.StudyInstanceUID, "");
+            request.Dataset.AddOrUpdate(DicomTag.StudyID, "");
+
+            // add the dicom tags that contain the filter criterias
+            request.Dataset.AddOrUpdate(DicomTag.PatientName, patientName);
+
+            return request;
+        }
+
+
+        public static DicomCFindRequest CreateSeriesRequestByStudyUID(string studyInstanceUID)
+        {
+            var request = new DicomCFindRequest(DicomQueryRetrieveLevel.Series);
+
+            request.Dataset.AddOrUpdate(new DicomTag(0x8, 0x5), "ISO_IR 100");
+
+            // add the dicom tags with empty values that should be included in the result
+            request.Dataset.AddOrUpdate(DicomTag.StudyDate, "");
+            request.Dataset.AddOrUpdate(DicomTag.StudyTime, "");
+            request.Dataset.AddOrUpdate(DicomTag.AccessionNumber, "");
+            request.Dataset.AddOrUpdate(DicomTag.ModalitiesInStudy, "");
+            request.Dataset.AddOrUpdate(DicomTag.StudyDescription, "");
+            request.Dataset.AddOrUpdate(DicomTag.InstitutionName, "");
+            request.Dataset.AddOrUpdate(DicomTag.ReferringPhysicianName, "");
+            request.Dataset.AddOrUpdate(DicomTag.ProtocolName, "");
+            request.Dataset.AddOrUpdate(DicomTag.PatientName, "");
+            request.Dataset.AddOrUpdate(DicomTag.PatientID, "");
+            request.Dataset.AddOrUpdate(DicomTag.PatientBirthDate, "");
+            request.Dataset.AddOrUpdate(DicomTag.PatientSex, "");
+            request.Dataset.AddOrUpdate(DicomTag.StudyInstanceUID, "");
+            request.Dataset.AddOrUpdate(DicomTag.StudyID, "");
+            request.Dataset.AddOrUpdate(DicomTag.SeriesInstanceUID, "");
+            request.Dataset.AddOrUpdate(DicomTag.SeriesDescription, "");
+            request.Dataset.AddOrUpdate(DicomTag.SeriesDate, "");
+            request.Dataset.AddOrUpdate(DicomTag.SeriesTime, "");
+            request.Dataset.AddOrUpdate(DicomTag.NumberOfSeriesRelatedInstances, "");
+            request.Dataset.AddOrUpdate(DicomTag.SeriesNumber, "");
+
+            // add the dicom tags that contain the filter criterias
+            request.Dataset.AddOrUpdate(DicomTag.StudyInstanceUID, studyInstanceUID);
+
+            return request;
+        }
+
+        public static void HandleResponse(DicomCFindRequest request, DicomCFindResponse response)
+        {
+            if (response.Status == DicomStatus.Pending)
+            {
+                // print the results
+                Console.WriteLine($"Patient {response.Dataset.Get<String>(DicomTag.PatientName)}, {response.Dataset.Get<String>(DicomTag.ModalitiesInStudy,-1)}-Study from {response.Dataset.Get<DateTime>(DicomTag.StudyDate)} with UID {response.Dataset.Get<String>(DicomTag.StudyInstanceUID)} ");
+            }
+            if (response.Status == DicomStatus.Success)
+            {
+                Console.WriteLine(response.Status.ToString());
+            }
+        }
+
+    }
+}
