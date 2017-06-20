d26e28de5780a20e5940dc6b5c52271cddfb86f6
Reinhard Gruber <gofal@users.noreply.github.com>
2017-06-20 07:45:07 +0200

    QR SCU sample (#10)
    * Upgraded NuGet packages for desktop to RC3.
    
    * Upgraded NuGet packages for Android to RC3.
    
    * Updated Mono packages.
    
    * Updated Mono assemblies to version 3.0.0.
    
    * Updated NuGet packages for desktop projects to 3.0.0
    
    * Display color image and upgrade NuGet package to 3.0.0.
    
    * Upgraded NuGet packages to 3.0.0 for Universal.
    
    * Updated README file.
    
    * Moved Mono project to separate folder.
    
    * Moved Android project to Android folder.
    
    * Made Android sample more comprehensive.
    
    * Move all desktop projects to separate folder.
    
    * Updated Mono project to account for Desktop folder moves.
    
    * Android sample use shared assembly info data.
    
    * Added DICOM Compare from main repository.
    
    * Documentation updates
    
    * Updated NLog to latest NuGet package.
    
    * Added stub for iOS sample project.
    
    * Added NuGet packages to iOS project
    
    * Added simple viewing of DICOM images and attributes.
    
    * Added support for setting the listener ports via command line argument.
    
    * Use latest fo-dicom, and build Release against .NET Native tool chain.
    
    * Upgraded NuGet packages for desktop to RC3.
    
    * Upgraded NuGet packages for Android to RC3.
    
    * Updated Mono packages.
    
    * Updated Mono assemblies to version 3.0.0.
    
    * Updated NuGet packages for desktop projects to 3.0.0
    
    * Display color image and upgrade NuGet package to 3.0.0.
    
    * Upgraded NuGet packages to 3.0.0 for Universal.
    
    * Moved Mono project to separate folder.
    
    * Moved Android project to Android folder.
    
    * Made Android sample more comprehensive.
    
    * Move all desktop projects to separate folder.
    
    * Updated Mono project to account for Desktop folder moves.
    
    * Android sample use shared assembly info data.
    
    * Added DICOM Compare from main repository.
    
    * Updated NLog to latest NuGet package.
    
    * Added stub for iOS sample project.
    
    * Added NuGet packages to iOS project
    
    * Added simple viewing of DICOM images and attributes.
    
    * Use latest fo-dicom, and build Release against .NET Native tool chain.
    
    * resolving issue https://github.com/fo-dicom/fo-dicom-samples/issues/4
    prividing the SOPInstanceUID if it doesn't provided by the request
    PrintSCP
    
    * cfind studies sample
    
    * cfind series, cget
    
    * CMove
    
    * cmove, including sample of a error case
    
    * remove temporary merge-file
    
    * adapted changes as requested in pull request
    
    * Delete packages.config.merge

---
+++ Desktop/QueryRetrieve SCU/Program.cs
@@ -0,0 +1,227 @@
+﻿// Copyright (c) 2012-2017 fo-dicom contributors.
+// Licensed under the Microsoft Public License (MS-PL).
+
+using Dicom;
+using Dicom.Network;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace QueryRetrieve_SCU
+{
+    class Program
+    {
+
+        private static string StoragePath = @".\DICOM";
+        // values of the Query Retrieve Server to test with.
+        private static string QRServerHost = "www.dicomserver.co.uk";
+        private static int QRServerPort = 104;
+        private static string QRServerAET = "STORESCP";
+        private static string AET = "FODICOMSCU";
+
+
+
+        static void Main(string[] args)
+        {
+            var client = new DicomClient();
+            client.NegotiateAsyncOps();
+
+            // Find a list of Studies
+
+            var request = CreateStudyRequestByPatientName("Tester^P*");
+
+            var studyUids = new List<string>();
+            request.OnResponseReceived += (req, response) =>
+            {
+                DebugStudyResponse(response);
+                studyUids.Add(response.Dataset?.Get<string>(DicomTag.StudyInstanceUID));
+            };
+            client.AddRequest(request);
+            client.Send(QRServerHost, QRServerPort, false, AET, QRServerAET);
+
+            // find all series from a study that previous was returned
+
+            var studyUID = studyUids[3];
+            request = CreateSeriesRequestByStudyUID(studyUID);
+            var serieUids = new List<string>();
+            request.OnResponseReceived += (req, response) =>
+            {
+                DebugSerieResponse(response);
+                serieUids.Add(response.Dataset?.Get<string>(DicomTag.SeriesInstanceUID));
+            };
+            client.AddRequest(request);
+            client.Send(QRServerHost, QRServerPort, false, AET, QRServerAET);
+
+            // now get all the images of a serie with cGet in the same association
+
+            var cGetRequest = CreateCGetBySeriesUID(studyUID, serieUids.First());
+            client.OnCStoreRequest += (DicomCStoreRequest req) =>
+            {
+                SaveImage(req.Dataset);
+                return new DicomCStoreResponse(req, DicomStatus.Success);
+            };
+            // the client has to accept storage of the images. We know that the requested images are of SOP class Secondary capture, 
+            // so we add the Secondary capture to the additional presentation context
+            // a more general approach would be to mace a cfind-request on image level and to read a list of distinct SOP classes of all
+            // the images. these SOP classes shall be added here.
+            client.AdditionalPresentationContexts.Add(DicomPresentationContext.GetScpRolePresentationContext(
+                DicomUID.SecondaryCaptureImageStorage, DicomTransferSyntax.ImplicitVRBigEndian,
+                DicomTransferSyntax.ExplicitVRBigEndian, DicomTransferSyntax.ExplicitVRLittleEndian));
+            client.AddRequest(cGetRequest);
+            client.Send(QRServerHost, QRServerPort, false, AET, QRServerAET);
+
+            // if the images shall be sent to an existing storescp and this storescp is configured on the QR SCP then a CMove could be performed:
+
+            // here we want to see how a error case looks like - because the test QR Server does not know the node FODICOMSCP
+            var cMoveRequest = CreateCMoveBySeriesUID("FODICOMSCP", studyUID, serieUids.First());
+            bool? moveSuccessfully = null;
+            cMoveRequest.OnResponseReceived += (DicomCMoveRequest requ, DicomCMoveResponse response) =>
+            {
+                if (response.Status.State == DicomState.Pending)
+                {
+                    Console.WriteLine("Sending is in progress. please wait");
+                }
+                else if (response.Status.State == DicomState.Success)
+                {
+                    Console.WriteLine("Sending successfully finished");
+                    moveSuccessfully = true;
+                }
+                else if (response.Status.State == DicomState.Failure)
+                {
+                    Console.WriteLine("Error sending datasets: " + response.Status.Description);
+                    moveSuccessfully = false;
+                }
+                Console.WriteLine(response.Status);
+            };
+            client.AddRequest(cMoveRequest);
+            client.Send(QRServerHost, QRServerPort, false, AET, QRServerAET);
+
+            if (moveSuccessfully.GetValueOrDefault(false))
+            {
+                // images sent successfully from QR Server to the store scp
+            }
+            Console.ReadLine();
+        }
+
+
+        public static DicomCFindRequest CreateStudyRequestByPatientName(string patientName)
+        {
+            // there is a built in function to create a Study-level CFind request very easily: 
+            // return DicomCFindRequest.CreateStudyQuery(patientName: patientName);
+
+            // but consider to create your own request that contains exactly those DicomTags that
+            // you realy need pro process your data and not to cause unneccessary traffic and IO load:
+
+            var request = new DicomCFindRequest(DicomQueryRetrieveLevel.Study);
+
+            // always add the encoding
+            request.Dataset.AddOrUpdate(new DicomTag(0x8, 0x5), "ISO_IR 100");
+
+            // add the dicom tags with empty values that should be included in the result of the QR Server
+            request.Dataset.AddOrUpdate(DicomTag.PatientName, "");
+            request.Dataset.AddOrUpdate(DicomTag.PatientID, "");
+            request.Dataset.AddOrUpdate(DicomTag.ModalitiesInStudy, "");
+            request.Dataset.AddOrUpdate(DicomTag.StudyDate, "");
+            request.Dataset.AddOrUpdate(DicomTag.StudyInstanceUID, "");
+            request.Dataset.AddOrUpdate(DicomTag.StudyDescription, "");
+
+            // add the dicom tags that contain the filter criterias
+            request.Dataset.AddOrUpdate(DicomTag.PatientName, patientName);
+
+            return request;
+        }
+
+
+        public static DicomCFindRequest CreateSeriesRequestByStudyUID(string studyInstanceUID)
+        {
+            // there is a built in function to create a Study-level CFind request very easily: 
+            // return DicomCFindRequest.CreateSeriesQuery(studyInstanceUID);
+
+            // but consider to create your own request that contains exactly those DicomTags that
+            // you realy need pro process your data and not to cause unneccessary traffic and IO load:
+            var request = new DicomCFindRequest(DicomQueryRetrieveLevel.Series);
+
+            request.Dataset.AddOrUpdate(new DicomTag(0x8, 0x5), "ISO_IR 100");
+
+            // add the dicom tags with empty values that should be included in the result
+            request.Dataset.AddOrUpdate(DicomTag.SeriesInstanceUID, "");
+            request.Dataset.AddOrUpdate(DicomTag.SeriesDescription, "");
+            request.Dataset.AddOrUpdate(DicomTag.Modality, "");
+            request.Dataset.AddOrUpdate(DicomTag.NumberOfSeriesRelatedInstances, "");
+
+            // add the dicom tags that contain the filter criterias
+            request.Dataset.AddOrUpdate(DicomTag.StudyInstanceUID, studyInstanceUID);
+
+            return request;
+        }
+
+
+        public static DicomCGetRequest CreateCGetBySeriesUID(string studyUID, string seriesUID)
+        {
+            var request = new DicomCGetRequest(studyUID, seriesUID);
+            // no more dicomtags have to be set
+            return request;
+        }
+
+
+        public static DicomCMoveRequest CreateCMoveBySeriesUID(string destination, string studyUID, string seriesUID)
+        {
+            var request = new DicomCMoveRequest(destination, studyUID, seriesUID);
+            // no more dicomtags have to be set
+            return request;
+        }
+
+
+        public static void DebugStudyResponse(DicomCFindResponse response)
+        {
+            if (response.Status == DicomStatus.Pending)
+            {
+                // print the results
+                Console.WriteLine($"Patient {response.Dataset.Get<String>(DicomTag.PatientName)}, {response.Dataset.Get<String>(DicomTag.ModalitiesInStudy, -1)}-Study from {response.Dataset.Get<DateTime>(DicomTag.StudyDate)} with UID {response.Dataset.Get<String>(DicomTag.StudyInstanceUID)} ");
+            }
+            if (response.Status == DicomStatus.Success)
+            {
+                Console.WriteLine(response.Status.ToString());
+            }
+        }
+
+
+        public static void DebugSerieResponse(DicomCFindResponse response)
+        {
+            try
+            {
+                if (response.Status == DicomStatus.Pending)
+                {
+                    // print the results
+                    Console.WriteLine($"Serie {response.Dataset.Get<String>(DicomTag.SeriesDescription)}, {response.Dataset.Get<String>(DicomTag.Modality)}, {response.Dataset.Get<int>(DicomTag.NumberOfSeriesRelatedInstances)} instances");
+                }
+                if (response.Status == DicomStatus.Success)
+                {
+                    Console.WriteLine(response.Status.ToString());
+                }
+            }
+            catch (Exception ex)
+            { }
+        }
+
+
+        public static void SaveImage(DicomDataset dataset)
+        {
+            var studyUid = dataset.Get<string>(DicomTag.StudyInstanceUID);
+            var instUid = dataset.Get<string>(DicomTag.SOPInstanceUID);
+
+            var path = Path.GetFullPath(Program.StoragePath);
+            path = Path.Combine(path, studyUid);
+
+            if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+
+            path = Path.Combine(path, instUid) + ".dcm";
+
+            new DicomFile(dataset).Save(path);
+        }
+
+
+    }
+}
