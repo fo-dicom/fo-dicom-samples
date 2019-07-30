444936c7155208337f264e9ccffe2377c17eae18
Reinhard Gruber <gofal3@live.com>
2019-07-31 00:38:43 +0200

    update to fo-dicom 4.0.2, use new DicomClient class

--- Desktop/C-Store SCU/Program.cs
+++ Desktop/C-Store SCU/Program.cs
@@ -5,152 +5,154 @@ using System;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading.Tasks;
 using Dicom.Network;
+using DicomClient = Dicom.Network.Client.DicomClient;
 
 namespace Dicom.CStoreSCU
 {
-   class Program
-   {
-      private static string StoreServerHost = "www.dicomserver.co.uk";
-      private static int StoreServerPort = 11112;
-      private static string StoreServerAET = "STORESCP";
-      private static string AET = "FODICOMSCU";
+    internal static class Program
+    {
 
-      static void Main(string[] args)
-      {
-         var client = new DicomClient();
-         client.NegotiateAsyncOps();
-         var storeMore = "";
+        private static string StoreServerHost = "www.dicomserver.co.uk";
+        private static int StoreServerPort = 11112;
+        private static readonly string StoreServerAET = "STORESCP";
+        private static readonly string AET = "FODICOMSCU";
 
-         StoreServerHost = GetServerHost();
-         StoreServerPort = GetServerPort();
+        static async Task Main(string[] args)
+        {
+            var storeMore = "";
 
-         Console.WriteLine("***************************************************");
-         Console.WriteLine("Server AE Title: " + StoreServerAET);
-         Console.WriteLine("Server Host Address: " + StoreServerHost);
-         Console.WriteLine("Server Port: " + StoreServerPort);
-         Console.WriteLine("Client AE Title: " + AET);
-         Console.WriteLine("***************************************************");
+            StoreServerHost = GetServerHost();
+            StoreServerPort = GetServerPort();
 
-         do
-         {
-            try
+            Console.WriteLine("***************************************************");
+            Console.WriteLine("Server AE Title: " + StoreServerAET);
+            Console.WriteLine("Server Host Address: " + StoreServerHost);
+            Console.WriteLine("Server Port: " + StoreServerPort);
+            Console.WriteLine("Client AE Title: " + AET);
+            Console.WriteLine("***************************************************");
+
+            var client = new DicomClient(StoreServerHost, StoreServerPort, false, AET, StoreServerAET);
+            client.NegotiateAsyncOps();
+
+            do
             {
-               Console.WriteLine();
-               Console.WriteLine("Enter the path for a DICOM file:");
-               Console.Write(">>>");
-               string dicomFile = Console.ReadLine();
+                try
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Enter the path for a DICOM file:");
+                    Console.Write(">>>");
+                    string dicomFile = Console.ReadLine();
 
-               while (!File.Exists(dicomFile))
-               {
-                  Console.WriteLine("Invalid file path, enter the path for a DICOM file or press Enter to Exit:");
+                    while (!File.Exists(dicomFile))
+                    {
+                        Console.WriteLine("Invalid file path, enter the path for a DICOM file or press Enter to Exit:");
 
-                  dicomFile = Console.ReadLine();
+                        dicomFile = Console.ReadLine();
 
-                  if (string.IsNullOrWhiteSpace(dicomFile))
-                  {
-                     return;
-                  }
-               }
+                        if (string.IsNullOrWhiteSpace(dicomFile))
+                        {
+                            return;
+                        }
+                    }
 
-               var request = new DicomCStoreRequest(dicomFile);
+                    var request = new DicomCStoreRequest(dicomFile);
 
-               request.OnResponseReceived += (req, response) =>
-               {
-                  Console.WriteLine("C-Store Response Received, Status: " + response.Status);
-               };
+                    request.OnResponseReceived += (req, response) =>
+                    {
+                        Console.WriteLine("C-Store Response Received, Status: " + response.Status);
+                    };
 
-               client.AddRequest(request);
-               client.Send(StoreServerHost, StoreServerPort, false, AET, StoreServerAET);
-            }
-            catch (Exception exception)
+                    await client.AddRequestAsync(request);
+                    await client.SendAsync();
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("----------------------------------------------------");
+                    Console.WriteLine("Error storing file. Exception Details:");
+                    Console.WriteLine(exception.ToString());
+                    Console.WriteLine("----------------------------------------------------");
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine("To store another file, enter \"y\"; Othersie, press enter to exit: ");
+                Console.Write(">>>");
+                storeMore = Console.ReadLine().Trim();
+
+            } while (storeMore.Length > 0 && storeMore.ToLower()[0] == 'y');
+        }
+
+        private static string GetServerHost()
+        {
+            var hostAddress = "";
+            var localIP = GetLocalIPAddress();
+            do
             {
-               Console.WriteLine();
-               Console.WriteLine("----------------------------------------------------");
-               Console.WriteLine("Error storing file. Exception Details:");
-               Console.WriteLine(exception.ToString());
-               Console.WriteLine("----------------------------------------------------");
-               Console.WriteLine();
-            }
+                Console.WriteLine("Your local IP is: " + localIP);
+                Console.WriteLine("Enter \"1\" to use your local IP Address: " + localIP);
+                Console.WriteLine("Enter \"2\" to use defult: " + StoreServerHost);
+                Console.WriteLine("Enter \"3\" to enter custom");
+                Console.Write(">>>");
 
-            Console.WriteLine("To store another file, enter \"y\"; Othersie, press enter to exit: ");
+                string input = Console.ReadLine().Trim().ToLower();
+
+                if (input.Length > 0)
+                {
+                    if (input[0] == '1')
+                    {
+                        hostAddress = localIP;
+                    }
+                    else if (input[0] == '2')
+                    {
+                        hostAddress = StoreServerHost;
+                    }
+                    else if (input[0] == '3')
+                    {
+                        Console.WriteLine("Enter Server Host Address:");
+                        Console.Write(">>>");
+
+                        hostAddress = Console.ReadLine();
+                    }
+                }
+            } while (hostAddress.Length == 0);
+
+
+            return hostAddress;
+        }
+
+        private static int GetServerPort()
+        {
+
+            Console.WriteLine("Enter Server port, or \"Enter\" for default \"" + StoreServerPort + "\":");
             Console.Write(">>>");
-            storeMore = Console.ReadLine().Trim();
 
-         } while (storeMore.Length > 0 && storeMore.ToLower()[0] == 'y');
-      }
+            var input = Console.ReadLine().Trim();
 
-      private static string GetServerHost()
-      {
-         var hostAddress = "";
-         var localIP = GetLocalIPAddress();
-         var input = "";
-
-         do
-         {
-            Console.WriteLine("Your local IP is: " + localIP);
-            Console.WriteLine("Enter \"1\" to use your local IP Address: " + localIP);
-            Console.WriteLine("Enter \"2\" to use defult: " + StoreServerHost);
-            Console.WriteLine("Enter \"3\" to enter custom");
-            Console.Write(">>>");
-
-            input = Console.ReadLine().Trim().ToLower();
-
-            if (input.Length > 0)
+            if (string.IsNullOrEmpty(input))
             {
-               if (input[0] == '1')
-               {
-                  hostAddress = localIP;
-               }
-               else if (input[0] == '2')
-               {
-                  hostAddress = StoreServerHost;
-               }
-               else if (input[0] == '3')
-               {
-                  Console.WriteLine("Enter Server Host Address:");
-                  Console.Write(">>>");
-
-                  hostAddress = Console.ReadLine();
-               }
+                return StoreServerPort;
             }
-         } while (hostAddress.Length == 0);
-
-
-         return hostAddress;
-      }
-
-      private static int GetServerPort()
-      {
-
-         Console.WriteLine("Enter Server port, or \"Enter\" for default \"" + StoreServerPort + "\":");
-         Console.Write(">>>");
-
-         var input = Console.ReadLine().Trim();
-
-         if (string.IsNullOrEmpty(input))
-         {
-            return StoreServerPort;
-         }
-         else
-         {
-            return int.Parse(input);
-         }
-      }
-
-      public static string GetLocalIPAddress()
-      {
-         var host = Dns.GetHostEntry(Dns.GetHostName());
-
-         foreach (var ip in host.AddressList)
-         {
-            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            else
             {
-               return ip.ToString();
+                return int.Parse(input);
             }
-         }
+        }
 
-         return "";
-      }
-   }
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+
+            return "";
+        }
+    }
 }
