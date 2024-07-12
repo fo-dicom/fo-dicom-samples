af3af225d7bc3c4005a91982f890ff5f325a00a7
Reinhard Gruber <r.gruber@smartinmedia.com>
2024-07-12 14:08:35 +0200

    update to latest version of fo-dicom 5.1.3

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,4 +1,4 @@
-// Copyright (c) 2012-2023 fo-dicom contributors.
+// Copyright (c) 2012-2024 fo-dicom contributors.
 // Licensed under the Microsoft Public License (MS-PL).
 
 #if WIN32
@@ -12,7 +12,7 @@ using System.Resources;
 
 [assembly: AssemblyCompany("fo-dicom")]
 [assembly: AssemblyProduct("fo-dicom-samples")]
-[assembly: AssemblyCopyright("Copyright © fo-dicom contributors 2012-2023")]
+[assembly: AssemblyCopyright("Copyright © fo-dicom contributors 2012-2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
